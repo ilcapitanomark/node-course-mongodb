bd68ae46970ad445cc08b38652d0c4faf29db438
Marco Piccioni <Marco.Piccioni@compart.com>
2019-01-10 11:11:16 +0100

    Changed a message

--- playground/mongodb-Find.js
+++ playground/mongodb-Find.js
@@ -53,7 +53,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
             console.log('Now fetching Users BY Matteo');
             console.log(JSON.stringify(docs, undefined,2));
         }, (err) => {
-            console.log('Unable to fetch Users', err);
+            console.log('Unable to fetch Users that are not Matteo', err);
         });
         
     //client.close();
