214e834c58b8d7dae65819380c413bd9720af6c1
Marco Piccioni <Marco.Piccioni@compart.com>
2019-01-10 12:11:52 +0100

    Added 3 delete methods

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,54 @@
+const {MongoClient, ObjectID} = require('mongodb');   //this line equal the above
+
+var obj = new ObjectID();  //this is creating new ObjectID
+
+console.log(obj, `Created on ${obj.getTimestamp()}`);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if(err){
+        return console.log('Unable to connect to mongodb server');
+    }
+    console.log('Connected to Mongodb Server');
+    const db = client.db('TodoApp');
+
+//deleteMnay
+db.collection('Users').deleteMany({
+    name: 'Marco',
+    age: 57,
+    location: 'BB'
+}, (err, result) => {
+    if(err){
+        return console.log('Unable to insert user');
+    }
+    console.log(result);
+});
+
+//deleteOne
+db.collection('Users').deleteOne({
+    name: 'Ivan',
+    age: 57,
+    location: 'BB'
+}, (err, result) => {
+    if(err){
+        return console.log('Unable to insert user');
+    }
+    console.log(result);
+});
+
+//findAndDelete
+db.collection('Users').findOneAndDelete({
+    name: 'Matteo',
+    age: 57,
+    location: 'BB'
+}, (err, result) => {
+    if(err){
+        return console.log('Unable to insert user');
+    }
+    console.log(result);
+});
+
+//db.close();
+
+
+client.close();
+});
