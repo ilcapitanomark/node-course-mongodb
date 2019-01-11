d752e3424f31e0564b62899fbbef49272a3bf3e2
Marco Piccioni <Marco.Piccioni@compart.com>
2019-01-11 11:59:19 +0100

    added findOneAndUpdate

---
+++ playground/mongodb-update.js
@@ -0,0 +1,28 @@
+const {MongoClient, ObjectID} = require('mongodb');   //this line equal the above
+
+var obj = new ObjectID();  //this is creating new ObjectID
+
+console.log(obj, `Created on ${obj.getTimestamp()}`);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if(err){
+        return console.log('Unable to connect to mongodb server');
+    }
+    console.log('Connected to Mongodb Server');
+    const db = client.db('TodoApp');
+
+    //findAndDelete
+    db.collection('Todos').findOneAndUpdate(
+            { text: "Call Lorenzo"}, 
+            { $set: {"completed" : true}},
+            { returnOriginal: true},
+            ( err, result) => {
+        if(err){
+            return console.log('Unable to update Todos');
+        }
+        console.log(result);
+    });
+
+
+client.close();
+});
