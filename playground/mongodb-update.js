16c6fda5bf6445e765532b459bfab154008aa8e6
Marco Piccioni <Marco.Piccioni@compart.com>
2019-01-11 12:09:01 +0100

    added incremet a number in update

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -23,6 +23,31 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
         console.log(result);
     });
 
+        //findOneAndUpdate
+        db.collection('Users').findOneAndUpdate(
+            { name: "Solange"}, 
+            { $set: {"age" : 13}},
+            { returnOriginal: true},
+            ( err, result) => {
+        if(err){
+            return console.log('Unable to update Todos');
+        }
+        console.log(result);
+    });
+
+            //findOneAndUpdate
+            // as filter I could use the objectId like this:
+            // {"_id" : ObjectId("5c35f7e4652ae2c275946a65")}
+            db.collection('Users').findOneAndUpdate(
+                { name: "Francesco"}, 
+                { $inc: {"age" : +1}},
+                { returnOriginal: false},
+                ( err, result) => {
+            if(err){
+                return console.log('Unable to update Todos');
+            }
+            console.log(result);
+        });
 
 client.close();
 });
