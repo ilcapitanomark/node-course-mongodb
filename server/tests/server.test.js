d3dfb3cc04569caa0e9a879228e25d1c579ab93c
Marco Piccioni <Marco.Piccioni@compart.com>
2019-01-14 14:46:23 +0100

    added test for bad request

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -53,7 +53,7 @@ describe('POST /todos', () => {
 
                 Todo.find().then((todos) => {
                     expect(todos.length).toBe(0);
-                    done();
+                    //done();
                 }).catch((e) => {
                     done(e);
                 });
