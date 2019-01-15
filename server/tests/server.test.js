3351482be3a849efb3ee8e52411418177a6b7c7c
Marco Piccioni <Marco.Piccioni@compart.com>
2019-01-15 10:44:51 +0100

    added Get Todos and Tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -61,3 +61,38 @@ describe('POST /todos', () => {
 
     });
 });
+
+describe('GET /todos', () => {
+    
+    it('should get all todos', (done) =>{
+        var text = 'Test todo text';
+        request(app)
+        .post('/todos')
+        .send({text})
+        .end((err, response) => {
+            if(err){
+                return done(err);
+            }
+            //console.log(response.body);
+        });
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((response) => {
+                expect(response.body.todos.length).toBe(1)
+              })    
+            .end((err, response) => {
+                if (err) {
+                    return done(err);
+                }
+                //console.log(response.body);
+                Todo.find().then((todos) => {
+                    expect(response.body.todos.length).toBe(1);
+                    //expect(response.body.todos[0].text).toBe(text);
+                    done();
+                }).catch((e) => {
+                    done(e);
+                });
+            });
+    });
+});
